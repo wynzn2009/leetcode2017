a6952bdffba610129b684f84d7be8a82861a6f4c
wynzn2009 <wangyuning0229@126.com>
2017-05-01 20:56:37 +0800

    init

---
+++ src/com/prisbox/one/Base.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.prisbox.one;
+
+/**
+ * @author LO
+ *
+ */
+public class Base {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
