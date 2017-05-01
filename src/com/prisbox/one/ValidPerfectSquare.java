e1fc32926e40ac39087c9f4066a5f4d94566c1b4
wynzn2009 <wangyuning0229@126.com>
2017-05-02 00:20:05 +0800

    367. Valid Perfect Square

---
+++ src/com/prisbox/one/ValidPerfectSquare.java
@@ -0,0 +1,41 @@
+/**
+ * 
+ */
+package com.prisbox.one;
+
+/**
+ * @author LO
+ *
+ */
+public class ValidPerfectSquare {
+	public boolean isPerfectSquare(int num) {
+        int last = num%10;
+        if(last==2||last==3||last==7||last==8){
+        	return false;
+        }
+        int min = 0;
+        int max = 46341;
+        int mid;
+        while(min!=max-1&&min!=max){
+        	mid = (min+max)/2;
+        	int temp = mid*mid;
+        	if(num==temp){
+        		return true;
+        	}else if(num<temp){
+        		max = mid;
+        	}else{
+        		min = mid;
+        	}
+        }
+        return false;
+    }
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ValidPerfectSquare v = new ValidPerfectSquare();
+		v.isPerfectSquare(2147395600);
+	}
+
+}
