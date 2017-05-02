8d8fcea17b6d0ea2b070c5010dc06dd52bf66f42
wynzn2009 <wangyuning0229@126.com>
2017-05-02 14:34:45 +0800

    400. Nth Digit

---
+++ src/com/prisbox/one/NthDigit.java
@@ -0,0 +1,52 @@
+/**
+ * 
+ */
+package com.prisbox.one;
+
+/**
+ * @author admin
+ *
+ */
+public class NthDigit {
+	public int findNthDigit(int n) {
+        if(n<=9){
+        	return n;
+        }else{
+        	n = n - 9;
+        }
+        long index = 2L;
+        long count = 9L;
+        long num = 10L;
+        long total;
+        while(true){
+        	count *= 10;
+        	total = count * index;
+        	if(n>total){
+        		n -= total;
+        		index++;
+        		num *=10;
+        	}else{
+        		int t = (int) ((n-1)%index);
+        		int x = (int) (num+(n-1)/index);
+        		String s = String.valueOf(x);
+        		if(t>=s.length()){
+        			return 0;
+        		}else{
+        			
+        			return s.charAt(t)-'0';
+        		}
+        	}
+        }
+    }
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		NthDigit n = new NthDigit();
+		int t = n.findNthDigit(1000);
+		System.out.println(t);
+	}
+
+}
