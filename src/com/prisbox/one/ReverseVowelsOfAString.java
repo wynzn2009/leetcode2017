a51de6b35ae5037fb21e566c7470217fb009e7d3
wynzn2009 <wangyuning0229@126.com>
2017-05-03 10:49:18 +0800

    345. Reverse Vowels of a String

---
+++ src/com/prisbox/one/ReverseVowelsOfAString.java
@@ -0,0 +1,49 @@
+/**
+ * 
+ */
+package com.prisbox.one;
+
+/**
+ * @author admin
+ *
+ */
+public class ReverseVowelsOfAString {
+	public String reverseVowels(String s) {
+        char[] c = s.toCharArray();
+        int l = 0;
+        int r = c.length-1;
+        String str = "aeiouAEIOU";
+        int count = 0;
+        while(l<r){
+        	if(str.contains(String.valueOf(c[l]))){
+        		count++;
+        	}else{
+        		l++;
+        	}
+        	if(str.contains(String.valueOf(c[r]))){
+        		count++;
+        	}else{
+        		r--;
+        	}
+        	if(count==2){
+        		c[l] = (char) (c[l]^c[r]);
+        		c[r] = (char) (c[l]^c[r]);
+        		c[l] = (char) (c[l]^c[r]);
+        		l++;
+        		r--;
+        	}
+        	count = 0;
+        }
+        return String.valueOf(c);
+    }
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ReverseVowelsOfAString r= new ReverseVowelsOfAString();
+		String leetcode = "leetcode";
+		System.out.println(r.reverseVowels(leetcode));
+	}
+
+}
