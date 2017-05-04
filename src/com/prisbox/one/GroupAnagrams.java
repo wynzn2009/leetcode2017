3a810386e1f5afb526a44ba6414d3ce065c37a47
wynzn2009 <wangyuning0229@126.com>
2017-05-04 08:39:14 +0800

    49. Group Anagrams

---
+++ src/com/prisbox/one/GroupAnagrams.java
@@ -0,0 +1,42 @@
+/**
+ * 
+ */
+package com.prisbox.one;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author admin
+ *
+ */
+public class GroupAnagrams {
+	public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String,List<String>> map = new HashMap<String,List<String>>();
+        for(String str : strs){
+        	char[] array = str.toCharArray();
+        	Arrays.sort(array);
+        	String key = String.valueOf(array);
+        	if(map.containsKey(key)){
+        		map.get(key).add(str);
+        	}else{
+        		List<String> list = new ArrayList<>();
+        		list.add(str);
+        		map.put(key, list);
+        	}
+        }
+        List<List<String>> l = new ArrayList<List<String>>(map.values());
+        return l;
+    }
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
