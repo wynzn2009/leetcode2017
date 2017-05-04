dd1239f7f0faca4dfbce11a8bcd42930ce2767db
wynzn2009 <wangyuning0229@126.com>
2017-05-04 12:29:59 +0800

    541. Reverse String II

---
+++ src/com/prisbox/one/ReverseStringII.java
@@ -0,0 +1,50 @@
+/**
+ * 
+ */
+package com.prisbox.one;
+
+/**
+ * @author admin
+ *
+ */
+public class ReverseStringII {
+	public String reverseStr(String s, int k) {
+        if(s.length()<=1||k<=1){
+        	return s;
+        }
+        char[] ch = s.toCharArray();
+        for(int i=0;i<(s.length()-1)/(2*k)+1;i++){
+        	int t = i*2*k;
+        	int e = t + k-1;
+        	if(e>=s.length()){
+        		e = s.length()-1;
+        	}
+        	help(ch,t,e);
+        }
+		return String.valueOf(ch);
+    }
+	
+	private void help(char[] s,int start,int end){
+		if(start>=end){
+			return;
+		}
+		while(start<end){
+			s[start] = (char) (s[start]^s[end]);
+			s[end] = (char) (s[start]^s[end]);
+			s[start] = (char) (s[start]^s[end]);
+			start++;
+			end--;
+		}
+	}
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ReverseStringII r = new ReverseStringII();
+		String s = "abcdefg";
+		int k = 2;
+		r.reverseStr(s, k);
+	}
+
+}
