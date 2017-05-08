ea19b15bb932d00dfcefd7e8ec989f208a875942
wynzn2009 <wangyuning0229@126.com>
2017-05-08 09:27:48 +0800

    41. First Missing Positive

---
+++ src/com/prisbox/one/FirstMissingPositive.java
@@ -0,0 +1,42 @@
+/**
+ * 
+ */
+package com.prisbox.one;
+
+/**
+ * @author admin
+ *
+ */
+public class FirstMissingPositive {
+	public int firstMissingPositive(int[] nums) {
+        for(int i=0;i<nums.length;i++){
+        	int t = nums[i];
+        	if(t==i+1){
+        		continue;
+        	}
+        	if(t>nums.length){
+        		nums[i] = 0;
+        		continue;
+        	}else if(t>0&&t!=nums[t-1]){
+        		nums[i] = nums[i]^nums[t-1];
+        		nums[t-1] = nums[i]^nums[t-1];
+        		nums[i] = nums[i]^nums[t-1];
+        		i--;
+        	}
+        }
+        for(int i=0;i<nums.length;i++){
+        	if(nums[i]!=i+1){
+        		return i+1;
+        	}
+        }
+        return nums.length+1;
+    }
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
