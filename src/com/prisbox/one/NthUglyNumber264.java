6691f3153294bfdd996d6e91d6348123e485a999
prisbox <wangyuning0229@163.com>
2018-04-10 00:20:38 +0800

    264

---
+++ src/com/prisbox/one/NthUglyNumber264.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+
+package com.prisbox.one;
+
+
+/**
+ * @author LO
+ *
+ */
+public class NthUglyNumber264 {
+
+    public int nthUglyNumber(int n) {
+
+    }
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
