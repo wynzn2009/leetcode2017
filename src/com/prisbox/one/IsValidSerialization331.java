d86d16473912ef8ea66869905855be3429bab2d9
prisbox <wangyuning0229@163.com>
2018-04-09 23:38:57 +0800

    331

--- src/com/prisbox/one/IsValidSerialization331.java
+++ src/com/prisbox/one/IsValidSerialization331.java
@@ -1,6 +1,7 @@
 /**
  * 
  */
+
 package com.prisbox.one;
 
 /**
@@ -8,16 +9,39 @@ package com.prisbox.one;
  *
  */
 public class IsValidSerialization331 {
-	public boolean isValidSerialization(String preorder) {
 
-	}
+    public boolean isValidSerialization(String preorder) {
+        if(preorder.startsWith("#")){
+            return preorder.length() == 1;
+        }
+        String[] t = preorder.split(",");
+        int a = 0;
+        int b = 0;
+        // int seqa = 0;
+        for(int i = 0; i < t.length; i++){
+            String current = t[i];
+            if(current.equals("#")){
+                b++;
+            }else{
+                a++;
+            }
+            if(b == a + 1 && i != t.length - 1){
+                return false;
+            }
+        }
+        if(b == a + 1){
+            return true;
+        }else{
+            return false;
+        }
+    }
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
 
-	}
+    }
 
 }
