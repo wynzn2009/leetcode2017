a7fd5574ff329b76fb3f58c3a67579b0c484cb37
wynzn2009 <wangyuning0229@126.com>
2018-04-10 13:15:14 +0800

    383

---
+++ src/com/prisbox/CanConstruct383.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package com.prisbox;
+
+/**
+ * @author admin
+ *
+ */
+public class CanConstruct383 {
+	public boolean canConstruct(String ransomNote, String magazine) {
+		if (ransomNote.length() > magazine.length()) {
+			return false;
+		}
+		int[] t = new int[26];
+		for (int i = 0; i < magazine.length(); i++) {
+			int index = magazine.charAt(i) - 'a';
+			t[index] = t[index] + 1;
+		}
+		for (int j = 0; j < ransomNote.length(); j++) {
+			int index = ransomNote.charAt(j) - 'a';
+			if (t[index] <= 0) {
+				return false;
+			} else {
+				t[index] = t[index] - 1;
+			}
+		}
+		return true;
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
