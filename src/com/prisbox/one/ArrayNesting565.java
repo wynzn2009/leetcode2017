787e23211865f050b39158e1ad4ef52264785ec7
wynzn2009 <wangyuning0229@126.com>
2018-04-11 13:01:02 +0800

    565

---
+++ src/com/prisbox/one/ArrayNesting565.java
@@ -0,0 +1,43 @@
+/**
+ * 
+ */
+package com.prisbox.one;
+
+/**
+ * @author admin
+ *
+ */
+public class ArrayNesting565 {
+	public static int arrayNesting(int[] nums) {
+		int max = 0;
+		int length = 0;
+		int index = 0;
+		for (int i = 0; i < nums.length; i++) {
+			if (nums[i] < 0) {
+				continue;
+			} else {
+				index = nums[i];
+				nums[i] = -1;
+				while (index != i) {
+					length++;
+					int temp = index;
+					index = nums[index];
+					nums[temp] = -1;
+				}
+				max = Math.max(max, length + 1);
+				length = 0;
+			}
+		}
+		return max;
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] a = {5,4,0,3,1,6,2};
+		arrayNesting(a);
+	}
+
+}
