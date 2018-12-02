86281067ff8e51e7b7af8a2a3d977765c7b19006
prisbox <wangyuning0229@163.com>
2018-12-02 22:12:47 +0800

    372

---
+++ src/com/prisbox/one/SuperPow372.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.prisbox.one;
+
+/**
+ * @author LO
+ *
+ *         你的任务是计算 ab 对 1337 取模，a 是一个正整数，b 是一个非常大的正整数且会以数组形式给出。
+ * 
+ *         示例 1:
+ * 
+ *         输入: a = 2, b = [3] 输出: 8 示例 2:
+ * 
+ *         输入: a = 2, b = [1,0] 输出: 1024
+ */
+public class SuperPow372 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
