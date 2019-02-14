7cf5f8d99df4badb16c8b8d3418a77882c057a03
wynzn29 <wynzn29@gmail.com>
2019-02-14 17:18:15 +0800

    575. 分糖果

---
+++ src/com/prisbox/one/DistributeCandies575.java
@@ -0,0 +1,18 @@
+package com.prisbox.one;
+
+import java.util.HashSet;
+
+public class DistributeCandies575 {
+	public int distributeCandies(int[] candies) {
+		int max = (candies.length + 1) / 2;
+		HashSet<Integer> set = new HashSet<>();
+		for (Integer i : candies) {
+			set.add(i);
+		}
+		return Math.min(max, set.size());
+	}
+
+	public static void main(String[] args) {
+
+	}
+}
