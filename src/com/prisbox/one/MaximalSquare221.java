6d8c06aee4db54059eaa4343b4ad387db1b81b2b
prisbox <wangyuning0229@163.com>
2019-05-12 18:37:14 +0800

    MaximalSquare221

--- src/com/prisbox/one/MaximalSquare221.java
+++ src/com/prisbox/one/MaximalSquare221.java
@@ -3,21 +3,48 @@
  */
 package com.prisbox.one;
 
-
 /**
  * @author LO
- * TODO 221
+ * 
+ *         在一个由 0 和 1 组成的二维矩阵内，找到只包含 1 的最大正方形，并返回其面积。
+ * 
+ *         示例:
+ * 
+ *         输入:
+ * 
+ *         1 0 1 0 0 1 0 1 1 1 1 1 1 1 1 1 0 0 1 0
+ * 
+ *         输出: 4
  */
 public class MaximalSquare221 {
-    public int maximalSquare(char[][] matrix) {
-        
-    }
-    /**
-     * @param args
-     */
-    public static void main(String[] args) {
-        // TODO Auto-generated method stub
+	public int maximalSquare(char[][] matrix) {
+		if (matrix.length < 1) {
+			return 0;
+		}
+		int m = matrix.length;
+		int n = matrix[0].length;
 
-    }
+		int[][] dp = new int[m + 1][n + 1];
+
+		int max = 0;
+		for (int i = 1; i <= m; i++) {
+			for (int j = 1; j <= n; j++) {
+				if (matrix[i - 1][j - 1] == '1') {
+
+					dp[i][j] = 1 + Math.min(dp[i - 1][j - 1], Math.min(dp[i - 1][j], dp[i][j - 1]));
+					max = Math.max(dp[i][j], max);
+				}
+			}
+		}
+		return max*max;
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
 
 }
