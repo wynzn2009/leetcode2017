b0cb828323d942aabb27a1f1a4d691d06324a1f4
prisbox <wangyuning0229@126.com>
2020-05-18 23:47:19 +0800

    389

---
+++ src/com/prisbox/one/FindtheDifference389.java
@@ -0,0 +1,45 @@
+/**
+ * 
+ */
+package com.prisbox.one;
+
+/**
+ * @author LO 给定两个字符串 s 和 t，它们只包含小写字母。
+ * 
+ *         字符串 t 由字符串 s 随机重排，然后在随机位置添加一个字母。
+ * 
+ *         请找出在 t 中被添加的字母。
+ * 
+ *          
+ * 
+ *         示例:
+ * 
+ *         输入： s = "abcd" t = "abcde"
+ * 
+ *         输出： e
+ * 
+ *         解释： 'e' 是那个被添加的字母。
+ * 
+ *         来源：力扣（LeetCode）
+ *         链接：https://leetcode-cn.com/problems/find-the-difference
+ *         著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class FindtheDifference389 {
+	public char findTheDifference(String s, String t) {
+		char[] re = (s + t).toCharArray();
+		int su = 0;
+		for (int i = 0; i < re.length; i++) {
+			su ^= re[i] - 'a';
+		}
+		return (char) ('a' + su);
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- src/com/prisbox/one/MaximumProductSubarray.java
+++ src/com/prisbox/one/MaximumProductSubarray.java
@@ -31,7 +31,7 @@ public class MaximumProductSubarray {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Integer.reverse(1);
 	}
 
 }
