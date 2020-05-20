64233d8b971f8deead35f0abe42ae5c0ad1a6e31
prisbox <wangyuning0229@126.com>
2020-05-20 22:30:20 +0800

    404

---
+++ src/com/prisbox/one/SumofLeftLeaves404.java
@@ -0,0 +1,56 @@
+/**
+ * 
+ */
+package com.prisbox.one;
+
+import java.util.LinkedList;
+
+import com.prisbox.base.TreeNode;
+
+/**
+ * @author LO 计算给定二叉树的所有左叶子之和。
+ * 
+ *         示例：
+ * 
+ *         3 / \ 9 20 / \ 15 7
+ * 
+ *         在这个二叉树中，有两个左叶子，分别是 9 和 15，所以返回 24
+ * 
+ *         来源：力扣（LeetCode）
+ *         链接：https://leetcode-cn.com/problems/sum-of-left-leaves
+ *         著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class SumofLeftLeaves404 {
+	public int sumOfLeftLeaves(TreeNode root) {
+		if(root==null) {
+			return 0;
+		}
+		int sum = 0;
+		LinkedList<TreeNode> l = new LinkedList<TreeNode>();
+		l.addLast(root);
+		while (!l.isEmpty()) {
+			TreeNode h = l.pollFirst();
+			if (h.left != null) {
+				if (h.left.left == null && h.left.right == null) {
+					sum += h.left.val;
+				} else {
+					l.addLast(h.left);
+				}
+			}
+			if (h.right != null) {
+				l.addLast(h.right);
+			}
+
+		}
+		return sum;
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
