129fe4e087e16ce779882aa59d063e8b7f6636dd
prisbox <wangyuning0229@126.com>
2020-05-25 22:10:39 +0800

    543

---
+++ src/com/prisbox/one/DiameterofBinaryTree543.java
@@ -0,0 +1,54 @@
+/**
+ * 
+ */
+package com.prisbox.one;
+
+import com.prisbox.base.TreeNode;
+
+/**
+ * @author LO 给定一棵二叉树，你需要计算它的直径长度。一棵二叉树的直径长度是任意两个结点路径长度中的最大值。这条路径可能穿过也可能不穿过根结点。
+ * 
+ *          
+ * 
+ *         示例 : 给定二叉树
+ * 
+ *         1 / \ 2 3 / \ 4 5 返回 3, 它的长度是路径 [4,2,1,3] 或者 [5,2,1,3]。
+ * 
+ *          
+ * 
+ *         注意：两结点之间的路径长度是以它们之间边的数目表示。
+ * 
+ *         来源：力扣（LeetCode）
+ *         链接：https://leetcode-cn.com/problems/diameter-of-binary-tree
+ *         著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class DiameterofBinaryTree543 {
+	int max = 0;
+
+	public int diameterOfBinaryTree(TreeNode root) {
+		if(root==null) {
+			return 0;
+		}
+		help(root);
+		return max;
+	}
+
+	public int help(TreeNode root) {
+		if (root == null) {
+			return 0;
+		}
+		int left = help(root.left);
+		int right = help(root.right);
+		max = Math.max(max, left + right);
+		return Math.max(left, right) + 1;
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
